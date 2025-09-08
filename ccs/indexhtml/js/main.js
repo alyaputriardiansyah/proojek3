01b5e4f624cde6132468c3369f323f91e5aaf85c
alyaputriardiansyah <alyaputriardiansyah@gmail.com>
2025-09-08 09:55:05 +0700

    Create main.js

---
+++ ccs/indexhtml/js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+      const ctaButton = document.querySelector('.cta');
+      if (ctaButton) {
+        ctaButton.addEventListener('click', () => {
+          alert('Selamat belajar Frontend Development!');
+        });
+      }
+    });
+  </script>
+</body>
+</html>
